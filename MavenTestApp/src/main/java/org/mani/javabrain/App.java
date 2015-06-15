2d6cc1e8c8aa40c87541ed79ecf65729e09e8515
root <root@ip-172-31-28-193.us-west-2.compute.internal>
2015-06-15 10:44:03 -0400

    First commit

---
+++ MavenTestApp/src/main/java/org/mani/javabrain/App.java
@@ -0,0 +1,13 @@
+package org.mani.javabrain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
