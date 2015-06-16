fed0c8e29fd1632c75df532197cc36ebb17e8c32
root <root@ip-172-31-28-193.us-west-2.compute.internal>
2015-06-16 09:36:58 -0400

    Merging

---
+++ src/main/java/org/mani/javabrain/Greeter.java
@@ -0,0 +1,6 @@
+package org.mani.javabrain;
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/org/mani/javabrain/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.mani.javabrain;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
