321e858c39766d42328976254439550f5a4ef537
root <root@ip-172-31-28-193.us-west-2.compute.internal>
2015-06-17 10:17:55 -0400

    App.java updated

--- src/main/java/org/mani/javabrain/HelloWorld.java
+++ src/main/java/org/mani/javabrain/HelloWorld.java
@@ -1,10 +1,7 @@
 package org.mani.javabrain;
-import org.slf4j.*;
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-       //System.out.println(greeter.sayHello());
-Logger logger = LoggerFactory.getLogger(App.class);
-logger.info("Hello World!");
+       System.out.println(greeter.sayHello());
     }
 }
